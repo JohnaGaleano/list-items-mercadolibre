a6476f9d87efd29da2bd5d5d9448c388fb77ab0c
johnagaleano <johnagaleano@gmail.com>
2019-08-09 12:59:11 -0500

    correciones de archivos y nombres

--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./styles.css";
 
-function CharacterCard(props) {
-  const { character } = props;
+function ProductCard(props) {
+  const { product } = props;
 
   return (
     <div
       className="CharacterCard"
-      style={{ backgroundImage: `url(${character.thumbnail})` }}
+      style={{ backgroundImage: `url(${product.thumbnail})` }}
     >
       <div className="CharacterCard__name-container text-truncate">
-        {character.title}
+        {product.title}
       </div>
     </div>
   );
@@ -43,13 +43,11 @@ class Search extends React.Component {
 
   handleChange = e => {
     this.setState({ item: e.target.value });
-    console.log(this.state.item);
   };
 
   handleClick = e => {
     this.setState(inicial);
     this.setState({ show: !this.state.show });
-    console.log("Button was clicked");
     console.log(this.state.data);
     
     this.fetchCharacters(this.state.item);
@@ -62,7 +60,6 @@ class Search extends React.Component {
         `https://api.mercadolibre.com/sites/MLU/search?q=${item}&access_token=APP_USR-6616257581210091-080916-88f6f9e74cdf6ee80085621ddf1a48dc-307302477`
       );
       const data = await response.json();
-      console.log(data);
         
       this.setState({
         loading: false,
@@ -78,8 +75,6 @@ class Search extends React.Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    // console.log("Form was submitted");
-    // console.log(this.state);
   };
   render() {
     return (
@@ -102,9 +97,9 @@ class Search extends React.Component {
 
         <div>
             <ul className="row">
-              {this.state.data.results.map(character => (
-                <li className="col-6 col-md-3" key={character.id}>
-                  <CharacterCard character={character} />
+              {this.state.data.results.map(product => (
+                <li className="col-6 col-md-3" key={product.id}>
+                  <ProductCard product={product} />
                 </li>
               ))}
             </ul>
