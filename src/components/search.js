dafffac0434c4c74206c934b3140a7c3380e2a2e
johnagaleano <johnagaleano@gmail.com>
2019-08-09 21:15:06 -0500

    mejoras

--- src/components/search.js
+++ src/components/search.js
@@ -13,7 +13,8 @@ function ProductCard(props) {
         {product.title}
       </div>
       <div className="ProductCard__price-container text-truncate">
-        <p>$</p>{product.price}
+        <p>$</p>
+        {product.price}
       </div>
       <div className="ProductCard__seller-container text-truncate">
         {product.seller}
@@ -21,15 +22,12 @@ function ProductCard(props) {
     </div>
   );
 }
+
 let info = [];
 
 const inicial = {
-  show: false,
   item: "",
-  loading: true,
-  error: null,
   data: {
-    info: {},
     results: []
   },
   nextPage: 1
@@ -38,13 +36,9 @@ const inicial = {
 class Search extends React.Component {
   state = {
     item: "",
-    loading: true,
-    error: null,
     data: {
-      info: {},
       results: []
-    },
-    nextPage: 1
+    }
   };
 
   handleChange = e => {
@@ -55,67 +49,35 @@ class Search extends React.Component {
     this.setState(inicial);
     this.fetchProducts(this.state.item);
   };
+
   fetchProducts = async item => {
     info = [];
-    this.setState({ loading: false, error: null });
-
     fetch(
       `https://api.mercadolibre.com/sites/MLU/search?q=${item}&access_token=APP_USR-6616257581210091-081000-b19e961dc11253afcf5910372627e572-307302477`
     )
       .then(res => res.json())
-      .then(json => {
-        console.log(json.results);
-
+      .then(data => {
         Promise.all(
-          json.results.map(element =>
-            fetch(
-              `https://api.mercadolibre.com/users/${element.seller.id}`
-            ).then(res =>
-              res.json().then(res => {
+          data.results.map(element =>
+            fetch(`https://api.mercadolibre.com/users/${element.seller.id}`)
+              .then(res => res.json())
+              .then(res => {
                 info.push({
                   title: element.title,
                   thumbnail: element.thumbnail,
                   price: element.price,
                   seller: res.nickname
                 });
-                
               })
-            )
-            
           )
-          
-        ).then(res => {
+        ).then(() => {
           this.setState({
-          loading: false,
-          data: {
-            results: [].concat(this.state.data.results, info)
-          },
-          nextPage: this.state.nextPage + 1
+            data: {
+              results: [].concat(this.state.data.results, info)
+            }
+          });
         });
-        console.log(this.state.data.results);})
-        
       });
-
-    //////////////////////////////
-    // try {
-    //   const response = await fetch(
-    //     `https://api.mercadolibre.com/sites/MLU/search?q=${item}&access_token=APP_USR-6616257581210091-080916-88f6f9e74cdf6ee80085621ddf1a48dc-307302477`
-    //   );
-    //   const data = await response.json();
-
-    //   this.setState({
-    //     loading: false,
-    //     data: {
-    //       info: data,
-    //       results: [].concat(this.state.data.results, data.results)
-    //     },
-    //     nextPage: this.state.nextPage + 1
-    //   });
-    // } catch (error) {
-    //   this.setState({ loading: false, error: error });
-    // }
-    ///////////////////////////
-    console.log("info2: " + info);
   };
 
   handleSubmit = e => {
